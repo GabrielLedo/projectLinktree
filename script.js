a7e9b174647eee3593524fb4069ceac78ea43396
Gabriel Ledo <gabrielledo977@gmail.com>
2023-10-06 22:28:37 -0300

    nothing

--- script.js
+++ script.js
@@ -4,18 +4,14 @@ function toggleMode() {
   //pegar tag img
   const img = document.querySelector("#profile img")
 
-  //substituir a imgem
-  if(html.classList.contains('light')) {
+  //substituir a imagem
+  if (html.classList.contains("light")) {
     // se tiver light mode, adicionar imagem light
-    img.setAttribute(
-      "src",
-      "./assets/assets/avatarLinkedin.png"
-    )
-  } else{
+    img.setAttribute("src", "./assets/assets/avatarLinkedin.png")
+  } else {
     // se tiver sem light mode, manter imagem normal
     img.setAttribute("src", "./assets/assets/avatar-light.png")
   }
 
   html.classList.toggle('light')
-  
 }
